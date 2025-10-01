30241412bfa6267dfb3c25fb84e814b728e99831
FaiziCodeSpace <faizanwebdev1@gmail.com>
2025-10-01 23:34:20 +0500

    Initial commit

---
+++ UI/src/app/employees-table/employees-table.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employees-table',
+  imports: [],
+  templateUrl: './employees-table.html',
+  styleUrl: './employees-table.css'
+})
+export class EmployeesTable {
+
+}
---
+++ UI/src/app/landing-page/landing-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { EmployeesTable } from '../employees-table/employees-table';
+
+@Component({
+  selector: 'app-landing-page',
+  imports: [EmployeesTable],
+  templateUrl: './landing-page.html',
+  styleUrl: './landing-page.css'
+})
+export class LandingPage {
+
+}
